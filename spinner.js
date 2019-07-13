07888151bbc0f751ce6fd8b321277033eeccd316
Niket <48689909+Niket16@users.noreply.github.com>
2019-07-13 20:12:07 +0530

    Create spinner.js

---
+++ spinner.js
@@ -0,0 +1,7 @@
+alert('Hey! If you like it Plz upvote and comment also')
+function start(){
+    document.getElementById("main").style.animationPlayState = "running";
+}
+function stop(){
+    document.getElementById("main").style.animationPlayState = "paused";
+}
